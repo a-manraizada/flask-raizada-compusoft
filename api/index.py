3394d234480ac04e5becf691428589b98f75fc9c
amanraizada-bloktrek <aman.raizada@privateblok.ai>
2025-07-30 23:58:28 +0530

    register

--- api/index.py
+++ api/index.py
@@ -26,7 +26,7 @@ class UserData(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(120), nullable=False)
 
